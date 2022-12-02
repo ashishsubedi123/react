52fda56367637881577cbaf1060d5a7c550b1d8e
ashish subedi <ashishsubedi854@gmail.com>
2022-12-02 12:23:42 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from './Button'
+const App =()=>{
+    return<div>
+    
+    <h1 >my website</h1>
+    <h3>This is my first react web development class </h3>
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+    </div>
+    }
+export default App;    
---
+++ src/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const button = ()=>{
+    return<button className="button">Google</button>
+}
+export default button;
